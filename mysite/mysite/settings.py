636764bf27478d46cd5bc0465e1ebd76bee45f22
Abdulwahed Mansour <info@sweetlearn.com>
2020-04-10 23:09:00 +0300

    Postgresql setup

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'meters',
 ]
 
 MIDDLEWARE = [
@@ -72,13 +74,17 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangoDB',
+        'USER': 'postgres',
+        'PASSWORD': 'Gaza1950!',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
